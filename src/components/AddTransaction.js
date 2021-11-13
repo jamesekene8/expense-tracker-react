278e6db9056354ec61f9378a51a4ff440803b3d0
James Uguna <jamesekene8@gmail.com>
2021-11-13 14:52:06 +0100

    This is the first version of this app

---
+++ src/components/AddTransaction.js
@@ -0,0 +1,62 @@
+import React, { useState, useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+
+const AddTransaction = () => {
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState(0);
+
+  const { addTransaction } = useContext(GlobalContext);
+
+  const changeTextHandler = (e) => {
+    setText(e.target.value);
+  };
+  const changeAmountHandler = (e) => {
+    setAmount(e.target.value);
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    const newTransaction = {
+      id: Math.floor(Math.random() * 100000000),
+      text,
+      amount: +amount,
+    };
+    addTransaction(newTransaction);
+    setText("");
+    setAmount(0);
+  };
+
+  return (
+    <div>
+      <h3>Add new transaction</h3>
+      <form onSubmit={onSubmitHandler}>
+        <div className="form-control">
+          <label>Text</label>
+          <input
+            type="text"
+            id="text"
+            value={text}
+            onChange={changeTextHandler}
+            placeholder="Enter text..."
+          />
+        </div>
+        <div className="form-control">
+          <label>
+            Amount <br />
+            (negative - expense, positive - income)
+          </label>
+          <input
+            type="number"
+            id="amount"
+            value={amount}
+            onChange={changeAmountHandler}
+            placeholder="Enter amount..."
+          />
+        </div>
+        <button className="btn">Add transaction</button>
+      </form>
+    </div>
+  );
+};
+
+export default AddTransaction;
